582eecb188a93f8242792387a93f2137b909f898
yangjian <yangjian102621@gmail.com>
2017-09-19 19:15:02 +0800

    feat: 橡皮擦功能和添加涂鸦背景功能

--- libs/scrawl.js
+++ libs/scrawl.js
@@ -1,81 +1,161 @@
 /**
  * Created by yangjian on 17-9-18.
  */
-(function() {
+(function($) {
 
-	Array.prototype.contain = function(obj) {
-		for (var i = 0; i < this.length; i++) {
-			if (obj == this[i]) {
-				return true;
-			}
-		}
-		return false;
+	// 设置元素可用状态
+	$.fn.enable = function() {
+		$(this).addClass("active");
+		$(this).removeAttr("disabled");
 	}
 
-	Array.prototype.remove = function(obj) {
-		var _new = [];
-		for (var i = 0; i < this.length; i++) {
-			if (obj != this[i]) {
-				_new.push(this[i]);
-			}
-		}
-		return _new;
+	// 设置元素不可用状态
+	$.fn.disable = function() {
+		$(this).removeClass("active");
+		$(this).attr("disabled", true);
 	}
 
 	var Canvas = function(options) {
 
 		var configs = {
 			width : 360,
-			height : 300
+			height : 300,
+			save : function(data) {}
 		}; //默认配置
 		options = options || {};
-		for(var name in options) {
-			configs[name] = options[name];
-		}
-		var canvas = G(configs.canvasId); //画布
+		$.extend(configs, options);
+
+		var canvas = $("#"+configs.canvasId)[0]; //画布
+		canvas.width = configs.width;
+		canvas.height = configs.height;
 		var context = canvas.getContext("2d"); //绘图环境
-		var prevBtn = G("J_prevStep"); //上一步
-		var nextBtn = G("J_nextStep"); //下一步
+		context.lineCap = "round";　//设置线条两端为圆弧
+		context.lineJoin = "round";　//设置线条转折为圆弧
+		//设置默认颜色
+		setColor();
+		var $prevBtn = $("#J_prevStep"); //上一步
+		var $nextBtn = $("#J_nextStep"); //下一步
+		var $clearBtn = $("#J_clearBoard"); //清空画板
 		var drawing = false; //是否正在绘制
-		var prevSteps = []; //返回上一步操作
-		var nextSteps = []; //恢复下一步操作
+		var erasering = false; //是否正在擦除
+		var prevSteps = []; //返回上一步操作集合
+		var nextSteps = []; //恢复下一步操作集合
+
+		$("#picBoard").css({
+			width : configs.width + "px",
+			height : configs.height + "px"
+		});
 
 		// 事件绑定
 		canvas.onmousedown = startDrawing;
 		canvas.onmouseup = stopDrawing;
 		canvas.onmouseout = stopDrawing;
 		canvas.onmousemove = doDrawing;
-		prevBtn.onclick = gotoPrevStep;
-		nextBtn.onclick = gotoNextStep;
+		$prevBtn.on("click", gotoPrevStep);
+		$nextBtn.on("click", gotoNextStep);
+		$clearBtn.on("click", clearBoard);
 
+		// 清空设置
+		$("#clearSetting").on("click", function() {
+			context.lineWidth = 1;
+			setColor($(".colorBar span:first").data("color"));
+			context.shadowBlur = 0;
+			alert("画笔已重新初始化，请重新配置画笔。");
+		});
 
-		function G(id) {
-			return document.getElementById(id);
-		}
+		//上传背景图片
+		$("#J_canvas_bg").on("change", function() {
 
+			if ($("#picBoard img").length > 0) {
+				$("#picBoard img:eq(0)").attr("src", window.URL.createObjectURL(this.files[0]));
+				return;
+			}
+			var $img = '<img src="'+window.URL.createObjectURL(this.files[0])+'" width="'+configs.width+'" height="'+configs.height+'" />';
+			$("#picBoard").append($img);
+
+			// 激活删除背景按钮
+			$("#J_removeImg").enable();
+
+		});
+
+		// 删除背景图片
+		$("#J_removeImg").on("click", function() {
+			$("#picBoard").empty();
+			$(this).disable();
+		});
+
+		//保存图片
+		$('#J_saveImg').on("click", saveImage);
+
+		// 设置笔刷大小
+		$("#scrawl-main .brush-size").on("click", function() {
+
+			context.restore(); //恢复到canvas的上一个状态
+			context.lineWidth = parseInt($(this).text());
+			erasering = false;
+
+		});
+
+		// 设置笔触虚化
+		$("#scrawl-main .blur-size").on("click", function() {
+			context.shadowBlur = parseInt($(this).text());
+		});
+
+		// 橡皮擦功能
+		$("#scrawl-main .eraser-size").on("click", function() {
+
+			if (erasering == true) {
+				return;
+			}
+			erasering = true;
+			context.save(); //保存canvas状态
+			context.lineCap = "round";　//设置线条两端为圆弧
+			context.lineJoin = "round";　//设置线条转折为圆弧
+			context.lineWidth = 10;
+			context.globalCompositeOperation = "destination-out";
+
+		});
+
+		//设置颜色
+		$("#scrawl-main .colorBar span").on("click",function() {
+
+			$("#scrawl-main .colorBar .active").removeClass("active");
+			$(this).addClass("active");
+			setColor($(this).data("color"));
+		});
+
+		// 开始绘制
 		function startDrawing(e) {
 			drawing = true;
 			//记录上一步的数据
 			prevSteps.push(context.getImageData(0, 0, configs.width, configs.height));
-			addClass(prevBtn, "prevStepH");
-
 			// 创建一个新的绘图路径
 			context.beginPath();
 			// 把画笔移动到鼠标位置
-			context.moveTo(canvas.offsetLeft + e.pageX, canvas.offsetTop + e.pageY);
+			var offset = $(canvas).offset();
+			context.moveTo(e.pageX - offset.left, e.pageY - offset.top);
 		}
 
 		// 停止绘制
 		function stopDrawing() {
+
 			drawing = false;
+			//清空下一步的数据集合，从新开始记录
+			nextSteps = [];
+			$nextBtn.disable();
+			if (prevSteps.length == 1) {
+				$prevBtn.enable();
+				$clearBtn.enable();
+			}
 		}
 
 		//绘制图像
 		function doDrawing(e) {
 			if (drawing) {
 				// 找到鼠标最新位置
-				var x = e.pageX - canvas.offsetLeft;
-				var y = e.pageY - canvas.offsetTop;
+				var offset = $(canvas).offset();
+				var x = e.pageX - offset.left;
+				var y = e.pageY - offset.top;
 				// 画一条直线到鼠标最新位置
 				context.lineTo(x, y);
 				context.stroke();
@@ -87,14 +167,14 @@
 		 */
 		function gotoPrevStep() {
 			if (prevSteps.length > 0) {
-				var imgData = prevSteps.pop();
-				context.putImageData(imgData, 0, 0);
-				nextSteps.push(imgData);
-
-				addClass(nextBtn, "nextStepH");
+				//保存当前状态到下一步的操作历史库
+				nextSteps.push(context.getImageData(0, 0, configs.width, configs.height));
+				var popData = prevSteps.pop();
+				context.putImageData(popData, 0, 0);
+				$nextBtn.enable();
 
 				if (prevSteps.length == 0) {
-					removeClass(prevBtn, "prevStepH");
+					$prevBtn.disable();
 				}
 			}
 		}
@@ -104,56 +184,76 @@
 		 */
 		function gotoNextStep() {
 			if (nextSteps.length > 0) {
+				//保存当前状态到上一步的操作历史库
+				prevSteps.push(context.getImageData(0, 0, configs.width, configs.height));
 				var imgData = nextSteps.pop();
 				context.putImageData(imgData, 0, 0);
-				prevSteps.push(imgData);
-
-				addClass(prevBtn, "prevStepH");
+				$prevBtn.enable();
 
 				if (nextSteps.length == 0) {
-					removeClass(nextBtn, "nextStepH");
+					$nextBtn.disable();
 				}
 			}
 		}
 
 		/**
-		 * 添加 class
-		 * @param obj
-		 * @param className
+		 * 清空画板
 		 */
-		function addClass(obj, className) {
-			var _className = obj.className.split(" ");
-			if (_className.contain(className)) {
-				return;
-			}
-			obj.className = _className + " " + className;
+		function clearBoard() {
+			context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+			prevSteps = [];
+			nextSteps = [];
+
+			$prevBtn.disable();
+			$nextBtn.disable();
+			$clearBtn.disable();
 		}
 
 		/**
-		 * 删除 class
-		 * @param obj
-		 * @param className
+		 * 设置画笔颜色
+		 * @param color
 		 */
-		function removeClass(obj, className) {
-			var _className = obj.className.split(" ");
-			var newClass = _className.remove(className);
-			obj.className = newClass.join(" ");
+		function setColor(color) {
+			if (!color) {
+				color = $(".colorBar .active:eq(0)").data("color");
+			}
+			context.strokeStyle = color;
+			context.shadowColor = color;
 		}
 
-		//设置画笔样式
-		function setCanvasStyle() {
-			context.lineWidth = configs.lineWidth;
-			context.shadowBlur = configs.shadowBlur;
-			context.shadowColor = configs.lineColor;
-			context.strokeStyle = configs.lineColor;
+		/**
+		 * 保存图片
+		 */
+		function saveImage() {
+
+			if ($("#picBoard img").length > 0) {
+				var image = new Image();
+				image.src = $("#picBoard img:eq(0)").attr("src");
+				image.onload = function() {
+					context.save();
+					context.globalCompositeOperation = "destination-atop";
+					context.drawImage(this, 0, 0, configs.width, configs.height);
+					context.restore();
+
+					configs.save(canvas.toDataURL("image/png"));
+				}
+
+			} else {
+				configs.save(canvas.toDataURL("image/png"));
+			}
+		}
+
+
+		//要导出的API
+		Canvas.prototype = {
+
+			nextStep : gotoPrevStep,
+			prevStep : gotoNextStep,
+			setColor : setColor
+
 		}
 	}
 
-	//要导出的API
-	Canvas.prototype = {
-
-
-	}
 
 	window.Canvas = Canvas;
-})();
+})(jQuery);
